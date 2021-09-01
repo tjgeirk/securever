7922343c3970f1010263282e58e968223d16c86a
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2021-09-01 11:15:35 -0700

    Update securer.sh

--- securer.sh
+++ securer.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 # securver by TJGEIRK
-
-module_requirements=("unattended-updates" "ufw" "openvpn" "git" "python" "python3" "python3-pip")
-repo_requirements=("https://github.com/Nyr/openvpn-install/" "https://github.com/skeeto/endlessh")
-
 sec_patch{
-  passwd &&
-  passwd root &&
-  apt-get update &&
-  apt-get upgrade -y &&
-  apt-get autoremove -y;
+    passwd && passwd root &&
+    apt-get update && apt-get upgrade -y && apt-get autoremove -y;
 };
-
+assign_values(){
+    module_requirements=("unattended-updates" "ufw" "sslh" "git" "python" "python3" "python3-pip" 
+        "docker-ce" "openvpn" "wireguard-tools" "shadowsocks-libev");
+    repo_requirements=("https://github.com/Nyr/openvpn-install/" "https://github.com/Nyr/wireguard-install/" 
+        "https://github.com/skeeto/endlessh" );
+};
 git_repos{
-  for repo in "${repo_requirements[@]}";
-  do git clone $repo; 
-  done;
+    for repo in "${repo_requirements[@]}";
+    do git clone $repo; 
+    done;
 };
-  apt-get install $module_requirements
-
-
-
-
+set +x;
+sec_patch && assign_values;
+apt-get install $module_requirements && 
+git_repos;
