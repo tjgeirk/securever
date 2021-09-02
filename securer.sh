b720ae7f3cc02f70ed77e14eccbc3e33b159ba48
Timothy Geirk <tjgeirk@me.com>
2021-09-01 21:39:02 -0700

    securer.sh

--- securer.sh
+++ securer.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # securver by TJGEIRK
 
-module_requirements=("unattended-updates" "ufw" "openvpn" "git" "python" "python3" "python3-pip")
-repo_requirements=("https://github.com/Nyr/openvpn-install/" "https://github.com/skeeto/endlessh")
-
+dependencies{
+  module_requirements=("unattended-updates" "ufw" "openvpn" "git" "python" "python3" "python3-pip");
+repo_requirements=(“https://github.com/Nyr/openvpn-install/“ “https://github.com/skeeto/endlessh”);
+} ; dependencies ;
 sec_patch{
   passwd &&
   passwd root &&
@@ -11,13 +12,23 @@ sec_patch{
   apt-get upgrade -y &&
   apt-get autoremove -y;
 };
+  sec_patch &&
+  
+apt-get install $module_requirements;
+
 
 git_repos{
   for repo in "${repo_requirements[@]}";
   do git clone $repo; 
   done;
 };
-  apt-get install $module_requirements
+  git_repos ;
+
+
+
+
+
+
 
 
 
