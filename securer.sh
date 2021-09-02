d7434a9d560e0334369f91c47f72309eb47655ca
Timothy Geirk <tjgeirk@me.com>
2021-09-02 16:40:03 -0700

    Update

--- securer.sh
+++ securer.sh
@@ -1,55 +1,37 @@
 #!/bin/bash
 # securver by TJGEIRK
 
-dependencies{
-  module_requirements=("unattended-updates" "ufw" "openvpn" "git" "python" "python3" "python3-pip");
-repo_requirements=(“https://github.com/Nyr/openvpn-install/“ “https://github.com/skeeto/endlessh”);
-} ; dependencies ;
-sec_patch{
-    passwd && passwd root &&
-    apt-get update && apt-get upgrade -y && apt-get autoremove -y;
-};
-  sec_patch &&
-  
-apt-get install $module_requirements;
 
-
-git_repos{
-  for repo in "${repo_requirements[@]}";
-  do git clone $repo; 
-  done;
-};
-  git_repos ;
-
-
-
-
-
-
-
-
-
+port=“NEW SSH PORT HERE”
 
 assign_values(){
-    module_requirements=("unattended-updates" "ufw" "sslh" "git" "python" "python3" "python3-pip" 
-        "docker-ce" "openvpn" "wireguard-tools" "shadowsocks-libev");
-    repo_requirements=("https://github.com/Nyr/openvpn-install/" "https://github.com/Nyr/wireguard-install/" 
-        "https://github.com/skeeto/endlessh" );
-};
+    module_requirements=("unattended-updates" "ufw" "sslh" "git" "python" "python3" "python3-pip" "docker-ce" "openvpn" "wireguard-tools" "shadowsocks-libev");
+    repo_requirements=("https://github.com/Nyr/openvpn-install/" "https://github.com/Nyr/wireguard-install/" "https://github.com/skeeto/endlessh");
+}; set +x;  assign_values;
+sec_patch(){
+    apt-get update &&
+    apt-get full-upgrade -y;
+}; sec_patch;
 git_repos(){
     for repo in "${repo_requirements[@]}";
     do git clone $repo; 
     done;
-};
+};git_repos;
 access(){
-    echo "Port 4847" >> /etc/ssh/sshd_config;
-    echo "port 4847" >> /etc/ssh/ssh_config;
-    systemctl enable ssh;
+    echo "Port $port" >> /etc/ssh/sshd_config;
+    echo "port $port" >> /etc/ssh/ssh_config;
+    ufw allow $port && ufw enable;
+    systemctl daemon-reload;
+    systemctl restart ssh;
     service ssh start;
     ssh-keygen;
-};
-set +x;
-sec_patch && assign_values;
-apt-get install $module_requirements && 
-git_repos &&
-access;
+}; access; 
+endlessh_make(){
+    make ./endlessh &&
+    mv endlessh/endlessh /bin/endlessh;
+    mv endlessh/endlessh.service /etc/systemd/system/endlessh.service;
+    mkdir /etc/endlessh &&
+    printf %b “Port 22/nPort 2222” > /etc/endlessh/config &&
+    systemctl enable endlessh &&
+    service endlessh start;
+}; endlessh_make;
